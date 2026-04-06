94835e8de7d3961dd35e301e7e45aac71abbec38
phidaun <163899792+phidaun@users.noreply.github.com>
2026-04-06 09:16:54 +0500

    pwa setup

--- public/index.php
+++ public/index.php
@@ -6,7 +6,15 @@
     <title>PWA</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
-    <script src='script.js'></script>
+    <script src='script.js'>
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('sw.js').then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }).catch(function(error) {
+                console.log('Service Worker registration failed:', error);
+            });
+        }
+    </script>
 </head>
 
 <body>
---
+++ public/service-worker.js
@@ -0,0 +1,44 @@
+const CAHSE_NAME = "quizapp-cache";
+
+const ASSETS = [
+  "/",
+  "/index.php",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json",
+  "/questions.json",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    }),
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      cacheNames.map(function (cacheName) {
+        if (cacheName !== CACHE_NAME) {
+          return caches.delete(cacheName);
+        }
+      });
+    }),
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) return response;
+      return fetch(event.request).then(function (fetchResponse) {
+        return caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }),
+  );
+});
